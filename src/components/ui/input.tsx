d80136aa223ae5c502606fd3d587e96d033f9342
KUMAR BISHOJIT <bishojit@gmail.com>
2025-05-23 13:58:06 +0000

    Outline issue not fixed.

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -9,7 +9,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-4 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm", // h-10 to h-9, text-base to text-sm, ring-offset-2 to ring-offset-4
+          "flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-4 focus-visible:border-[hsl(var(--ring))] disabled:cursor-not-allowed disabled:opacity-50 md:text-sm", // h-10 to h-9, text-base to text-sm, ring-offset-2 to ring-offset-4, added focus-visible:border-[hsl(var(--ring))]
           className
         )}
         ref={ref}
--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -20,7 +20,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-4 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", // ring-offset-2 to ring-offset-4
+      "flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-4 focus-visible:border-[hsl(var(--ring))] disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1", // ring-offset-2 to ring-offset-4, added focus-visible:border-[hsl(var(--ring))]
       className
     )}
     {...props}
